f7bc8397ea95127ee647752a0fa3474165867aac
Quang Nguyen <quangnguyen@192.168.102.42>
2025-03-31 19:58:34 +0700

    update code 1

--- src/screens/Test2/index.tsx
+++ src/screens/Test2/index.tsx
@@ -1,16 +1,16 @@
 // import MapboxGL from '@react-native-mapbox-gl/maps';
-import React, {useContext, useEffect} from 'react';
-import {StyleSheet, View} from 'react-native';
-import {AppContext} from '../../provider/AppProvider';
+import React, { useContext, useEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import { AppContext } from "../../provider/AppProvider";
 // MapboxGL.setAccessToken(
 //   'pk.eyJ1IjoiYW50aWtlbiIsImEiOiJja3I1MDhqN3ozMTM1MnZxaGg4bWZlYndvIn0.PeYwMkO2KIs0FvXJg0PcqA',
 // );
 const Test2 = () => {
-  const {insetsDevice} = useContext(AppContext);
+  const { insetsDevice } = useContext(AppContext);
 
   return (
-    <View style={[styles.container, {paddingTop: insetsDevice.top}]}>
-      {/* <MapboxGL.MapView style={styles.map} /> */}
+    <View style={[styles.container, { paddingTop: insetsDevice.top }]}>
+      {/* <MapboxGL.MapView style={styles.map} /> */}2
     </View>
   );
 };
@@ -20,14 +20,14 @@ export default Test2;
 const styles = StyleSheet.create({
   page: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'tomato',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "tomato",
   },
   container: {
     height: 300,
     width: 300,
-    backgroundColor: 'tomato',
+    backgroundColor: "tomato",
   },
   map: {
     flex: 1,
